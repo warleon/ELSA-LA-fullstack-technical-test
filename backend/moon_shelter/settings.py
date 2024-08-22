e5bb0900393cb7707cc7b41218032028110dfcfd
warleon <51892390+warleon@users.noreply.github.com>
2024-08-22 10:58:23 -0500

    Update settings.py
    register all the apps

--- backend/moon_shelter/settings.py
+++ backend/moon_shelter/settings.py
@@ -40,7 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'shelters',
+    'animals',
+    'adoptions',
+    'users'
 ]
 
 MIDDLEWARE = [

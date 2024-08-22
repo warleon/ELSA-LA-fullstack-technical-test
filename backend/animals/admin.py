44912ef7dcd775d27b07423d0f2ab19ebfd134a7
warleon <51892390+warleon@users.noreply.github.com>
2024-08-22 10:56:43 -0500

    Update admin.py
    register animals models

--- backend/animals/admin.py
+++ backend/animals/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from animals.models import (Specie,Breed,Animal)
 
 # Register your models here.
+admin.site.register(Specie)
+admin.site.register(Breed)
+admin.site.register(Animal)

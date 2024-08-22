574fcd869129a1a662b8e980e0f92412cd4fcaaf
warleon <51892390+warleon@users.noreply.github.com>
2024-08-22 17:11:09 -0500

    Update serializers.py
    Specie and Breed serializers

--- backend/animals/serializers.py
+++ backend/animals/serializers.py
@@ -1,7 +1,15 @@
 
 from rest_framework import serializers
-from animals.models import Animal
+from animals.models import Animal,Specie,Breed
 
+class SpecieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Specie
+        fields = '__all__'
+class BreedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Breed
+        fields = '__all__'
 class AnimalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Animal

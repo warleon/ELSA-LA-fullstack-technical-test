ae2be5f859a4235c7820937cad36312f1b3252cb
warleon <51892390+warleon@users.noreply.github.com>
2024-08-22 11:54:25 -0500

    Update views.py
    shelters

--- backend/shelters/views.py
+++ backend/shelters/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+
+from shelters.models import Shelter
+from shelters.serializers import ShelterSerializer
+from common.pagination import Paginator
 
 # Create your views here.
+
+class ShelterListView(ListAPIView):
+    queryset = Shelter.objects.all()
+    serializer_class = ShelterSerializer
+    pagination_class = Paginator 

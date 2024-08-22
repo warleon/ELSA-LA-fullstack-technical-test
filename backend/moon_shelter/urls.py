4d2fbc1b8f98c9d444f425c82675620cea3265a5
warleon <51892390+warleon@users.noreply.github.com>
2024-08-22 17:29:39 -0500

    Update urls.py

--- backend/moon_shelter/urls.py
+++ backend/moon_shelter/urls.py
@@ -27,4 +27,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('shelters.urls')),
+    path('api/', include('animals.urls')),
+    path('api/', include('users.urls')),
+    path('api/', include('adoptions.urls')),
 ]

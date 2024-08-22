7c7346ea7b5462653011504fa6b28fea981b57a8
warleon <51892390+warleon@users.noreply.github.com>
2024-08-22 17:20:12 -0500

    Update views.py

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from common.genericCRUDView import GenericCRUDView
+from users.models import Person, Helper, Adopter
+from users.serializers import PersonSerializer, HelperSerializer, AdopterSerializer
 
 # Create your views here.
+class PersonCRUDView(GenericCRUDView):
+    model = Person
+    serializer_class = PersonSerializer 
+class HelperCRUDView(GenericCRUDView):
+    model = Helper
+    serializer_class = HelperSerializer 
+class AdopterCRUDView(GenericCRUDView):
+    model = Adopter
+    serializer_class = AdopterSerializer 

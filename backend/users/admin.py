fe4b1621860afd934ac56a8046d2abc49b7a74f0
warleon <51892390+warleon@users.noreply.github.com>
2024-08-22 10:57:40 -0500

    Update admin.py
    register users models

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from users.models import (Person,Helper,Adopter)
 
 # Register your models here.
+
+admin.site.register(Person)
+admin.site.register(Helper)
+admin.site.register(Adopter)

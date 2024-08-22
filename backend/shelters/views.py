2468908eae63a465f950265543838c70f33d3342
warleon <51892390+warleon@users.noreply.github.com>
2024-08-22 17:19:47 -0500

    Update views.py
    clean imports

--- backend/shelters/views.py
+++ backend/shelters/views.py
@@ -1,11 +1,6 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import get_object_or_404
-
+from common.genericCRUDView import GenericCRUDView
 from shelters.models import Shelter
 from shelters.serializers import ShelterSerializer
-from common.genericCRUDView import GenericCRUDView
 
 # Create your views here.
 class ShelterCRUDView(GenericCRUDView):

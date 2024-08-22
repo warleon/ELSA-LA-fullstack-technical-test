c5a3a16cfe3ad2f8b9ac451794e2eb29244178bf
warleon <51892390+warleon@users.noreply.github.com>
2024-08-22 17:11:32 -0500

    Update views.py
    create CRUD views for the animals related models

--- backend/animals/views.py
+++ backend/animals/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
 
+from animals.models import Animal,Specie,Breed
+from animals.serializers import AnimalSerializer, SpecieSerializer, BreedSerializer
+from common.genericCRUDView import GenericCRUDView
 # Create your views here.
+class SpecieCRUDView(GenericCRUDView):
+    model = Specie
+    serializer_class = SpecieSerializer
+class BreedCRUDView(GenericCRUDView):
+    model = Breed
+    serializer_class = BreedSerializer
+class AnimalCRUDView(GenericCRUDView):
+    model = Animal
+    serializer_class = AnimalSerializer

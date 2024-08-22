b80d2b4810bbfa709536a37e82045686ac00439c
warleon <51892390+warleon@users.noreply.github.com>
2024-08-22 17:29:10 -0500

    Update views.py

--- backend/adoptions/views.py
+++ backend/adoptions/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from common.genericCRUDView import GenericCRUDView
+from adoptions.models import Adoption
+from adoptions.serializers import AdoptionSerializer
 
 # Create your views here.
+
+class AdoptionCRUDView(GenericCRUDView):
+    model =  Adoption
+    serializer_class = AdoptionSerializer
